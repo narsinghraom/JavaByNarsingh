82b20954e77e96166c4429ed4c53bec5ffba9301
vanajanalla <vanaja.n.dhatriinfo@gmail.com>
2017-11-04 15:16:10 +0530

    logic class

--- CollaMedicalCloud1/src/colla/medical/logics/ConvertingList.java
+++ CollaMedicalCloud1/src/colla/medical/logics/ConvertingList.java
@@ -1,19 +1,35 @@
 package colla.medical.logics;
 
+import java.util.Iterator;
 import java.util.List;
 
 import colla.medical.bean.EmpBean;
+import colla.medical.bean.LoginBean;
 
-public class ConvertingList 
-{
-	public static EmpBean convertingToObject(List<EmpBean> empBeanList,String username,String password)
-	{
+public class ConvertingList {
+	
+	/*
+	 * converting a single employeeobject with database list object and client side value object.
+	*/
+	public static EmpBean loginListConvert(LoginBean loginBean, List<EmpBean> listEmp){
 		EmpBean empBean=null;
+		if(listEmp!=null && listEmp.size()>0){
+		Iterator<EmpBean> iterator=listEmp.iterator();
+		while(iterator.hasNext())
+		{
+			EmpBean empBeanTemp=(EmpBean)iterator.next();
+			if(loginBean !=null && empBeanTemp !=null)
+			{
+					if((empBeanTemp.getUsername().equals(loginBean.getUserName()))&&(empBeanTemp.getPassword().equals(loginBean.getPassword()))){
+						empBean=empBeanTemp;
+					}
+			}
+			
+			
+		}
 		
-		if(empBeanList)
-		
+		}
 		return empBean;
-		
 	}
 
 }
